30422729b058c4aa8470ce57bc3a6ef7ef8b16d5
Andris Shvarcs <andris.svarcs@gmail.com>
2017-03-27 22:29:31 +0100

    Adding JS Gulp Dev task

---
+++ wordpress/wp-content/themes/pa-mini/dev/js/bundle.js
@@ -0,0 +1,96 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	/*\_(ツ)_*/
+	
+	/* Story starts here */
+	/*
+	 Before appConfig size
+	 * bundle - 67kb
+	 * 	67kb
+	 * min - 29kb
+	 * 	33kb
+	 * */
+	
+	var _main = __webpack_require__(1);
+	
+	var _main2 = _interopRequireDefault(_main);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function ready(fn) {
+	    // is content loaded
+	    if (document.readyState != 'loading') {
+	        fn();
+	    } else {
+	        document.addEventListener('DOMContentLoaded', fn);
+	    }
+	}
+	
+	ready(function () {
+	    document.body.classList.remove('no-js');
+	    _main2.default.init();
+	});
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	exports.default = init;
+	function init() {
+	    console.log('ta-daa');
+	}
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
