20c603c0991aeaa4679bf6e7f696fa041b9bbfaa
Andris Shvarcs <andris.svarcs@gmail.com>
2017-03-29 22:17:11 +0100

    Testing HTML wiswyg widget, improvements for overall design

--- wordpress/wp-content/themes/pa-mini/footer.php
+++ wordpress/wp-content/themes/pa-mini/footer.php
@@ -1,25 +1,27 @@
 <?php
- /* The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package WordPress
- * @subpackage PA-mini-01
- */
+/* The template for displaying the footer.
+*
+* Contains the closing of the id=main div and all content after
+*
+* @package WordPress
+* @subpackage PA-mini-01
+*/
 ?>
-        </div>	<!-- End Main Area -->
-		<!--end container-->
-	</div>
+</div>    <!-- End Main Area -->
+<!--end container-->
+</div>
 
 
-	</div>
-	<!--Footer Information-->
-	<footer class="group">
+</div>
+<!--Footer Information-->
+<footer class="group">
+    <div class="_footer-inner">
         <?php pa_media_hook(); ?>
         <?php get_sidebar('footer'); ?>
-	</footer>
-	<!-- End Footer Information -->
-    <?php wp_footer(); ?>
+    </div>
+</footer>
+<!-- End Footer Information -->
+<?php wp_footer(); ?>
 </body>
 </html>
 
